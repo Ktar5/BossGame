fa30b2c52464fb2b8ee74effb1f8a3ad9109e205
biokuta7 <hatmelders@gmail.com>
2017-07-08 14:24:42 -0700

    mapping

---
+++ Assets/Editor/MapImporter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MapImporter : EditorWindow {
+
+	int tileWidth = 32;
+	int tileHeight = 32;
+	Material mat;
+
+	[MenuItem("Window/MapImporter")]
+	static void Init() {
+		MapImporter mapImporter = (MapImporter)EditorWindow.GetWindow (typeof(MapImporter));
+		mapImporter.Show ();
+	}
+
+	void OnGUI() {
+		GUILayout.Label("Map Importer", EditorStyles.boldLabel);
+		mat = (Material) EditorGUILayout.ObjectField ("Material", mat, typeof(Material), false);
+		Texture tex = mat.mainTexture;
+		int mapWidth = tex.width / tileWidth;
+		int mapHeight = tex.height / tileHeight;
+
+		GUILayout.Label (tex);
+		GUILayout.Label ("World space dimensions: " + mapWidth + " X " + mapHeight);
+
+
+		if (GUILayout.Button ("Create")) {
+			
+
+			GameObject map = GameObject.CreatePrimitive (PrimitiveType.Quad);
+			map.name = "MAP";
+			map.transform.position = Vector3.zero;
+			map.transform.localScale = new Vector3 (mapWidth, mapHeight, 1);
+			map.GetComponent<MeshRenderer> ().material = mat;
+
+		}
+
+	}
+
+}
---
+++ Assets/Resources/pkmn.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="pkmn" tilewidth="16" tileheight="16" tilecount="2912" columns="52">
+ <image source="tileset-wesley.png" width="832" height="896"/>
+</tileset>
